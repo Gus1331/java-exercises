727d8a5be235984df12353d6ccf0f1ed9289ea5e
Gus Oliver <gustavooliver.souza@gmail.com>
2023-02-08 22:27:51 -0300

    ex002

---
+++ ex002/src/ex02/Main.java
@@ -0,0 +1,12 @@
+package ex02;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world from intelliJ IDE!");
+        String Abc = "15 + 25 * 2 = ";
+        int A = 15;                             // variable type int
+        int B = 25;
+        int C = 2;
+        System.out.println(Abc + (A + B * C));          // print values and messages on console
+    }
+}
